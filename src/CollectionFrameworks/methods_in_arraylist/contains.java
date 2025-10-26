b1a50376bd351b1407ffabdf4ad49f606dbdb53a
Ayish Shaikh <ayishshaikh.8805@gmail.com>
2025-10-26 14:18:01 +0530

    contains committed in the recent repo

---
+++ src/CollectionFrameworks/methods_in_arraylist/contains.java
@@ -0,0 +1,23 @@
+package CollectionFrameworks.methods_in_arraylist;
+
+import java.util.ArrayList;
+
+public class contains {
+    //The contains() method returns true if an item exists in a list and false otherwise.
+    public static void main(String[] args) {
+        ArrayList<Integer> arraylist = new ArrayList<>();
+        arraylist.add(1);
+        arraylist.add(24);
+        arraylist.add(38);
+        System.out.println(arraylist.contains(24));//the output will be true cuz it belongs to arraylist
+        System.out.println(arraylist.contains(25));//the output will be false cuz it does not belong to arraylist
+
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        System.out.println(cars.contains("BMW"));
+        System.out.println(cars.contains("Toyota"));
+    }
+}
