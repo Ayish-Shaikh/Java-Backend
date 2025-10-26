0f36a37290ca4f94effa40f5926f9b2e6de63936
Ayish Shaikh <ayishshaikh.8805@gmail.com>
2025-10-26 14:11:57 +0530

    foreach commited in the recent repo

---
+++ src/CollectionFrameworks/methods_in_arraylist/foreach.java
@@ -0,0 +1,27 @@
+package CollectionFrameworks.methods_in_arraylist;
+
+import java.util.ArrayList;
+public class foreach {
+    //The forEach() method performs an action on every item in a list.
+    // The action can be defined by a lambda expression that is compatible with the accept() method of Java's Consumer interface.
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arraylist = new ArrayList<>();
+        arraylist.add(1);
+        arraylist.add(24);
+        arraylist.add(38);
+        //can write for each loop both ways
+        arraylist.forEach( (n) -> { System.out.println(n); } );// simply x is element here not a pointer so it will print the output accordingly.
+//        for(int n: arraylist){
+//            System.out.println(n);
+//        }
+
+        ArrayList<String> cars = new ArrayList<>();
+        cars.add("Volvo");
+        cars.add("Volkswagen");
+        cars.add("Mercedes");
+        for(String x: cars){
+            System.out.println(x);
+        }
+    }
+}
